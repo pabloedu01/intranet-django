aea04dabba0ff5247ebdd2f8bc3085c4e51dc2a9
pabloedu01 <pablo@pabloedu.com>
2022-12-08 13:15:16 -0300

    Title portal

--- intranet/urls.py
+++ intranet/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import RedirectView
+import os
+
+admin.site.site_header = os.environ.get('DJANGO_NAME_PORTAL')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
