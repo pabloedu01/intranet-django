eb991934d21969cda0d1ecb48084dc2aca9b117d
pabloedu01 <pablo@pabloedu.com>
2022-12-08 11:25:19 -0300

    .

--- intranet/urls.py
+++ intranet/urls.py
@@ -1,7 +1,7 @@
-"""poll URL Configuration
+"""intranet URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,21 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from questions.views import (question_detail , 
-save_question_result ,login_view , register_view ,
- dashboard ,create_poll , see_answers)
-
 
 urlpatterns = [
-    path('' , login_view , name="login"),
-    path('register/' , register_view , name="register_view"),
-    path('dashboard/' , dashboard , name="dashboard"),
-    path('create_poll/' , create_poll , name="create_poll"),
-path('see_answers/' , see_answers , name="see_answers"),
-
-
-
-    path('question/<question_uid>/' , question_detail , name="question_detail"),
-    path('api/save_question_result/' , save_question_result),
     path('admin/', admin.site.urls),
 ]
