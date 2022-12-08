8275317f16741777d9ef62981006e32fcfdc1a09
pabloedu01 <pablo@pabloedu.com>
2022-12-08 12:39:14 -0300

    add migrations financeiro

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,6 @@ echo " BUILD START"
 python3.9  -m pip install -r requirements.txt
 python3.9 manage.py collectstatic  --noinput --clear
 python3 manage.py makemigrations cadastros
+python3 manage.py makemigrations financeiro
 python3.9 manage.py migrate
 echo " BUILD END"
