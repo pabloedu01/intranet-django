4f34af5e07710fcb4059578148b8acb67114c158
pabloedu01 <pablo@pabloedu.com>
2022-12-08 11:50:40 -0300

    .

--- intranet/settings.py
+++ intranet/settings.py
@@ -73,12 +73,19 @@ WSGI_APPLICATION = 'intranet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': os.environ.get('DATABASE_DATABASE'),  
+        'USER': os.environ.get('DATABASE_USERNAME'),  
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),  
+        'HOST': os.environ.get('DATABASE_HOST'),  
+        'PORT': os.environ.get('DATABASE_PORT'),  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
+}  
 
 
 # Password validation
