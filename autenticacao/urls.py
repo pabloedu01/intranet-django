ac092068b8bb4cf042b66debcdc8f1cde50bb93f
pabloedu01 <pablo@pabloedu.com>
2022-12-08 11:00:25 -0300

    .

--- autenticacao/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('autenticacao/login',views.login_simple,name='login'),
-    path('autenticacao/logout',views.logout_view,name='logout'),
-    path('autenticacao/login_simple',views.login_simple,name='login_simple')
-]
--- intranet/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.decorators import login_required
-
-@login_required(login_url="/autenticacao/login")
-def indexPage(request):
-    return redirect('/dashboard_default')
