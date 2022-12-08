ef8ff7e5f58367a00fa1ff332ceb63873d114a53
pabloedu01 <pablo@pabloedu.com>
2022-12-08 12:02:05 -0300

    .

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,6 @@
 echo " BUILD START"
 python3.9  -m pip install -r requirements.txt
 python3.9 manage.py collectstatic  --noinput --clear
-# python3.9 manage.py collectstatic  --noinput --clear
+python3.9 manage.py makemigrations  --noinput --clear
+python3.9 manage.py migrate  --noinput --clear
 echo " BUILD END"
