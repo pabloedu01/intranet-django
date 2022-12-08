c93310892ea63105cd0c1c6db22533a1035725e6
pabloedu01 <pablo@pabloedu.com>
2022-12-08 11:10:31 -0300

    .

--- poll/asgi.py
+++ poll/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for intranet project.
+ASGI config for poll project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intranet.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poll.settings')
 
 application = get_asgi_application()
--- poll/wsgi.py
+++ poll/wsgi.py
@@ -1,17 +1,17 @@
 """
-WSGI config for intranet project.
+WSGI config for poll project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intranet.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poll.settings')
 
 application = get_wsgi_application()
 app = application
