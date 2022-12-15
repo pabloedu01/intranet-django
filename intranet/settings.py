63acebdebcdb7a48ec8f38f2f676f645694af73d
pabloedu01 <pablo@pabloedu.com>
2022-12-15 16:42:11 -0300

    .

--- intranet/settings.py
+++ intranet/settings.py
@@ -143,10 +143,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 SASS_PROCESSOR_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = 'media'
+STATIC_ROOT = 'static'
 
 STATICFILES_FINDERS = [
 'django.contrib.staticfiles.finders.FileSystemFinder',
