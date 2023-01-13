212fc54e42954201fb6fed6cac91879c1aecd005
pabloedu01 <pablo@pabloedu.com>
2023-01-13 10:55:08 -0300

    .

--- intranet/settings.py
+++ intranet/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = os.environ.get('HOST_AUTORIZATION')
 ALLOWED_HOSTS = ['*']
