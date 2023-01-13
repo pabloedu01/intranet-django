0c6470b29e84fab0946feb2be6396cbedb5f6252
pabloedu01 <pablo@pabloedu.com>
2023-01-13 10:59:20 -0300

    .

--- intranet/settings.py
+++ intranet/settings.py
@@ -154,7 +154,8 @@ STATIC_URL = 'static/'
 SASS_PROCESSOR_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = 'media'
-STATIC_ROOT = 'static'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATICFILES_FINDERS = [
 'django.contrib.staticfiles.finders.FileSystemFinder',
